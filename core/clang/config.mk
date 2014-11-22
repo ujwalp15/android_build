ba87750ba56cd01a408e0769cfcb229b87017225
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-11-22 02:57:25 +0100

    clang: add unknown compiler flags
    Change-Id: Ibf91da65d22afd4981a0197e02d99d3e04fc1a87

--- core/clang/config.mk
+++ core/clang/config.mk
@@ -52,7 +52,9 @@ CLANG_CONFIG_UNKNOWN_CFLAGS := \
   -Wmaybe-uninitialized \
   -Wno-maybe-uninitialized \
   -Wno-error=maybe-uninitialized \
-  -fno-canonical-system-headers
+  -fno-canonical-system-headers \
+  -fgcse-las \
+  -Wstrict-aliasing=3
 
 # Clang flags for all host rules
 CLANG_CONFIG_HOST_EXTRA_ASFLAGS :=
