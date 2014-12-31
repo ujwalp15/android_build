75120458a3a0ec156cf8b05d1d94a4c24c901446
Chet Kener <Cl3Kener@gmail.com>
2014-12-31 13:10:43 -0500

    armv7-a-neon: Apply Correct mfpu and mfloat tunings per arch
    * Before everything was neon and soft float.  Now let's allow for per cortex tuning. Benchmarks should go up.  See here for better references https://github.com/CyanogenMod/android_build/commit/8907b775376211e73a566e8c9ef65e5b76cba1af
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/combo/arch/arm/armv7-a-neon.mk
    
    Change-Id: I69f8c59f5aabca28ccb7b3a91bdbf0e056a46731

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,33 +7,28 @@ ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
 ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
 	# Fake ARM compiler flags as these processors support LPAE and VFP4 which GCC/clang
 	# doesn't advertise.
 	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1 -D__ARM_FEATURE_VFP4=1
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
+	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
+	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp
+	arch_variant_ldflags := -Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=softfp
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a5)
-	arch_variant_cflags := -mcpu=cortex-a5 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a5 -mfpu=neon-vfpv4 -mfloat-abi=softfp
 else
-	arch_variant_cflags := -march=armv7-a -mfpu=neon
+	arch_variant_cflags := -march=armv7-a -mfpu=neon -mfloat-abi=softfp
 endif
 endif
 endif
 endif
 endif
-
-arch_variant_cflags += \
-    -mfloat-abi=softfp
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
